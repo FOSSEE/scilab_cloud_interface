258852beab9862e026423ab19d91274e71609616
prashant <prashant@iitb-mhrd.(none)>
2015-10-01 16:58:28 +0530

    modified database connection settings according to new settings

--- db-connect.php
+++ db-connect.php
@@ -1,13 +1,10 @@
 <?php
 	require_once('../scilab_in_2015/sites/default/settings.php');
-	$db_data = str_replace("mysql://", "", $db_url['default']);
-	$tmp_db_data = explode(":", $db_data);
-	$db_user = $tmp_db_data[0];
-	$tmp_db_data = explode("@", $tmp_db_data[1]);
-	$db_pass = $tmp_db_data[0];
-	$tmp_db_data = explode("/", $tmp_db_data[1]);
-	$db_host = $tmp_db_data[0];
-	$db = $tmp_db_data[1];
+
+        $db = $databases['default']['default']['database'];
+	$db_host = $databases['default']['default']['host'];
+	$db_user = $databases['default']['default']['username'];
+	$db_pass = $databases['default']['default']['password'];
 	mysql_connect($db_host,$db_user,$db_pass) or die("Connection failed"); //open database
 	mysql_select_db($db) or die("Not Connected to Database"); //select database
 ?>
